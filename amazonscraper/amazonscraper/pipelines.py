d2217d01c7eaff470beeb25cc9160c15ce3ac941
A5TA <cardillojamesbrandon@gmail.com>
2022-06-07 16:12:54 -0400

    Made a SQlite database and used pipelines

--- amazonscraper/amazonscraper/pipelines.py
+++ amazonscraper/amazonscraper/pipelines.py
@@ -6,8 +6,37 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import sqlite3
 
 
 class AmazonscraperPipeline:
+    def __init__(self):
+        self.create_connection()
+        self.create_table()
+    
+    def create_connection(self):
+        self.conn = sqlite3.connect("products.db")
+        self.curr = self.conn.cursor()
+        
+    def create_table(self):
+        #create a table in sqlite
+        self.curr.execute("""DROP TABLE IF EXISTS products_tb""")
+        self.curr.execute("""CREATE TABLE products_tb(
+                        name text,
+                        reviews INTEGER,
+                        price INTEGER,
+                        url text
+                        )""")
+    
     def process_item(self, item, spider):
+        self.store_db(item)
         return item
+    
+    def store_db(self,item):
+        self.curr.execute("""INSERT INTO products_tb values (?,?,?,?) """,(
+            item['name'][0], 
+            item['reviews'][0],
+            item['price'][0],
+            item['url'][0]
+        ))
+        self.conn.commit()
--- amazonscraper/amazonscraper/settings.py
+++ amazonscraper/amazonscraper/settings.py
@@ -66,9 +66,9 @@ DOWNLOADER_MIDDLEWARES = {
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'amazonscraper.pipelines.AmazonscraperPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'amazonscraper.pipelines.AmazonscraperPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
